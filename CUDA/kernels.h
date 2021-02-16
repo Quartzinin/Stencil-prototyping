52a3be9bf1ac4329f8db98e0619dd5a1cc227d23
Jonathan WH <levestol@hotmail.com>
2021-02-16 18:07:19 +0100

    add 1d-global-mem

--- CUDA/kernels.h
+++ CUDA/kernels.h
@@ -3,7 +3,6 @@
 
 #include <cuda_runtime.h>
 
-template <int T>
 __global__
 void sevenPointStencil_single_iter_tiled_sliding(
         const float* A,
@@ -11,72 +10,8 @@ void sevenPointStencil_single_iter_tiled_sliding(
         const unsigned nx,
         const unsigned ny,
         const unsigned nz
-        )
-{
-    const float c0 = 1.0/6.0;
-    const float c1 = 1.0/6.0/6.0;
+        );
 
-    const unsigned lidz = blockIdx.x*blockDim.x + threadIdx.x;
-    const unsigned lidy = blockIdx.y*blockDim.y + threadIdx.y;
-    const unsigned total = ny*nz;
-
-    __shared__ float TILE[3][T][T+1];
-
-    //should current tile read from something 32 x 32, or write to something 32 x 32
-    //write something 32x32 by adding extra reads.!!!
-
-    // in this algorithm we do not write to the edges of the matrix
-
-    const unsigned yoff = lidy*nz;
-    if (lidz < nz && lidy < ny)
-    {
-        TILE[0][threadIdx.y][threadIdx.x] = A[lidz + yoff];
-    }
-
-    __syncthreads();
-
-    // for small y and z we should tune the schedule to utilize the parallelism of the x-axis
-    for (int i = 0; i < nx; ++i)
-    {	
-	const unsigned xoff = i*total;
-	const unsigned cx = i % 3;
-	const unsigned nx = (i+1) % 3;
-
-        if (lidz < nz && lidy < ny && i + 1 < nx)
-        {
-            TILE[nx][threadIdx.y][threadIdx.x] = A[lidz + yoff + xoff+total];
-        }
-        __syncthreads();
-        
-        if (lidz < nz && lidy < ny)
-        {
-            const float mid = TILE[cx][threadIdx.y][threadIdx.x];
-            float res;
-            if (i == 0 || i == nx-1 || lidy == 0 || lidy == ny-1 || lidz == 0 || lidz == nz-1)
-            {
-                res = mid;
-            }
-            else {
-                const float left_z  = (threadIdx.x == 0) ? A[(lidz-1) + yoff + xoff]
-                    			            : TILE[cx][threadIdx.y][threadIdx.x-1];
-                const float right_z = (threadIdx.x == T-1) ? A[(lidz+1) + yoff + xoff]
-                    			               : TILE[cx][threadIdx.y][threadIdx.x+1];
-                const float left_y  = (threadIdx.y == 0) ? A[lidz + yoff-nz + xoff]
-                    			             : TILE[cx][threadIdx.y-1][threadIdx.x];
-                const float right_y = (threadIdx.y == T-1) ? A[lidz + yoff+nz + xoff]
-                    			               : TILE[cx][threadIdx.y+1][threadIdx.x];
-                const float left_x  = TILE[(i-1) % 3][threadIdx.y][threadIdx.x];
-                const float right_x = TILE[nx][threadIdx.y][threadIdx.x];
-
-                res = left_z + right_z + left_y + right_y + left_x + right_x * c1 + mid * c0;
-            }
-            out[lidz + yoff + xoff] = res;
-        }
-    }
-
-}
-
-template <int T>
 __global__
 void sevenPointStencil_single_iter_tiled_sliding_read(
         const float* A,
@@ -84,56 +19,8 @@ void sevenPointStencil_single_iter_tiled_sliding_read(
         const unsigned nx,
         const unsigned ny,
         const unsigned nz
-        )
-{
-    const float c0 = 1.0/6.0;
-    const float c1 = 1.0/6.0/6.0;
+        );
 
-    const unsigned lidz = blockIdx.x*30 + threadIdx.x;
-    const unsigned lidy = blockIdx.y*30 + threadIdx.y;
-    const unsigned total = ny*nz;
-
-    __shared__ float TILE[3][T][T+1];
-
-    if (lidz < nz && lidy < ny)
-    {
-        TILE[0][threadIdx.y][threadIdx.x] = A[lidz+lidy*nz+0*total];
-    }
-
-    for (int i = 0; i < nx; ++i)
-    {
-        if (lidz < nz && lidy < ny && i + 1 < nx)
-        {
-            TILE[(i+1) % 3][threadIdx.y][threadIdx.x] = A[lidz+lidy*nz+(i+1)*total];
-        }
-        __syncthreads();
-        
-        if (lidz < nz && lidy < ny && threadIdx.x > 0 && threadIdx.x < T-1 && threadIdx.y > 0 && threadIdx.y < T-1)
-        {
-            const float mid = TILE[i % 3][threadIdx.y][threadIdx.x];
-            float res;
-            if (i == 0 || i == nx-1 || lidy == 0 || lidy == ny-1 || lidz == 0 || lidz == nz-1)
-            {
-                res = mid;
-            }
-            else {
-                const float left_z  = TILE[i % 3][threadIdx.y][threadIdx.x-1];
-                const float right_z = TILE[i % 3][threadIdx.y][threadIdx.x+1];
-                const float left_y  = TILE[i % 3][threadIdx.y-1][threadIdx.x];
-                const float right_y = TILE[i % 3][threadIdx.y+1][threadIdx.x];
-                const float left_x  = TILE[(i-1) % 3][threadIdx.y][threadIdx.x];
-                const float right_x = TILE[(i+1) % 3][threadIdx.y][threadIdx.x];
-
-                res = left_z + right_z + left_y + right_y + left_x + right_x * c1 + mid * c0;
-            }
-            out[lidz+lidy*nz+i*total] = res;
-        }
-    }
-
-}
-
-
-template <int T>
 __global__
 void sevenPointStencil_single_iter(
         const float* A,
@@ -141,45 +28,24 @@ void sevenPointStencil_single_iter(
         const unsigned nx,
         const unsigned ny,
         const unsigned nz
-        )
+        );
+
+template<int W>
+__global__
+void breathFirst(
+    const int* A,
+    int * out,
+    const unsigned nx
+    )
 {
-    const float c0 = 1.0/6.0;
-    const float c1 = 1.0/6.0/6.0;
+    const unsigned gid = blockIdx.x*blockDim.x + threadIdx.x;
 
-    const unsigned lidz = blockIdx.x*blockDim.x + threadIdx.x;
-    const unsigned lidy = blockIdx.y*blockDim.y + threadIdx.y;
-    const unsigned total = ny*nz;
-
-    //should current tile read from something 32 x 32, or write to something 32 x 32
-    //write something 32x32 by adding extra reads.!!!
-
-    // in this algorithm we do not write to the edges of the matrix
-
-    // for small y and z we should tune the schedule to utilize the parallelism of the x-axis
-    for (int i = 0; i < nx; ++i)
-    {   
-        if (lidz < nz && lidy < ny)
-        {
-            const float mid = A[lidz+lidy*nz+i*total];
-            float res;
-            if (i == 0 || i == nx-1 || lidy == 0 || lidy == ny-1 || lidz == 0 || lidz == nz-1)
-            {
-                res = mid;
-            }
-            else {
-                const float left_z  = A[(lidz-1) + lidy*nz+i*total];
-                const float right_z = A[(lidz+1) + lidy*nz+i*total];
-                const float left_y  = A[lidz + (lidy-1)*nz+i*total];
-                const float right_y = A[lidz + (lidy+1)*nz+i*total];
-                const float left_x  = A[lidz + lidy*nz+(i-1)*total];
-                const float right_x = A[lidz + lidy*nz+(i+1)*total];
-
-                res = left_z + right_z + left_y + right_y + left_x + right_x * c1 + mid * c0;
-            }
-            out[lidz+lidy*nz+i*total] = res;
-        }
+    if (gid < nx)
+    {
+        const unsigned li = (gid <= W) ? 0 : gid - W;
+        const unsigned hi = ((nx - W) <= gid) ? nx - 1 : gid + W;
+        out[gid] = A[li] + A[gid] + A[hi];
     }
-
 }
 
 #endif
--- CUDA/stencil.cu
+++ CUDA/stencil.cu
@@ -13,7 +13,7 @@ using std::endl;
 
 
 
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+static int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
 {
     unsigned int resolution=1000000;
     long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
@@ -23,7 +23,7 @@ int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval 
 }
 
 
-inline void cudAssert(cudaError_t exit_code,
+static inline void cudAssert(cudaError_t exit_code,
         const char *file,
         int         line) {
     if (exit_code != cudaSuccess) {
@@ -34,13 +34,13 @@ inline void cudAssert(cudaError_t exit_code,
 }
 #define CUDASSERT(exit_code) { cudAssert((exit_code), __FILE__, __LINE__); }
 
-void sevenPointStencil(
+static void sevenPointStencil(
         float * start,
         float * swap_out,
         const unsigned nx,
         const unsigned ny,
         const unsigned nz,
-        const unsigned iterations
+        const unsigned iterations // must be odd
         )
 {
     const int T = 32;
@@ -49,25 +49,25 @@ void sevenPointStencil(
     dim3 block(T,T,1);
     dim3 grid(dimx, dimy, 1);
 
-    for (int i = 0; i < iterations; ++i){
+    for (unsigned i = 0; i < iterations; ++i){
         if(i & 1){
-            sevenPointStencil_single_iter<T> <<< grid,block >>>(swap_out, start, nx, ny, nz);
+            sevenPointStencil_single_iter<<< grid,block >>>(swap_out, start, nx, ny, nz);
         }
         else {
-            sevenPointStencil_single_iter<T> <<< grid,block >>>(start, swap_out, nx, ny, nz);
+            sevenPointStencil_single_iter<<< grid,block >>>(start, swap_out, nx, ny, nz);
         }
     }
-    // we don't actually care to write the result to swap_out (in case the number of iterations is even)
+    CUDASSERT(cudaDeviceSynchronize());
 
 }
 
-void sevenPointStencil_tiledSliding(
+static void sevenPointStencil_tiledSliding(
         float * start,
         float * swap_out,
         const unsigned nx,
         const unsigned ny,
         const unsigned nz,
-        const unsigned iterations
+        const unsigned iterations // must be odd
         )
 {
     const int T = 32;
@@ -76,78 +76,90 @@ void sevenPointStencil_tiledSliding(
     dim3 block(T,T,1);
     dim3 grid(dimx, dimy, 1);
 
-    for (int i = 0; i < iterations; ++i){
+    for (unsigned i = 0; i < iterations; ++i){
         if(i & 1){
-            sevenPointStencil_single_iter_tiled_sliding<T> <<< grid,block >>>(swap_out, start, nx, ny, nz);
+            sevenPointStencil_single_iter_tiled_sliding <<< grid,block >>>(swap_out, start, nx, ny, nz);
         }
         else {
-            sevenPointStencil_single_iter_tiled_sliding<T> <<< grid,block >>>(start, swap_out, nx, ny, nz);
+            sevenPointStencil_single_iter_tiled_sliding <<< grid,block >>>(start, swap_out, nx, ny, nz);
         }
     }
-    // we don't actually care to write the result to swap_out (in case the number of iterations is even)
+    CUDASSERT(cudaDeviceSynchronize());
 
 }
-void sevenPointStencil_tiledSliding_read(
+static void sevenPointStencil_tiledSliding_fully(
         float * start,
         float * swap_out,
         const unsigned nx,
         const unsigned ny,
         const unsigned nz,
-        const unsigned iterations
+        const unsigned iterations // must be odd
         )
 {
-    const int T = 32;
-    const int Tu = 30;
-    const int dimx = (nz + (Tu-1))/Tu;
-    const int dimy = (ny + (Tu-1))/Tu;
-    dim3 block(T,T,1);
-    dim3 grid(dimx, dimy, 1);
+    const unsigned T = 32;
+    const unsigned Ts = 6;
+    const unsigned dimx = (nx + (T-1))/T;
+    const unsigned dimy = (ny + (Ts-1))/Ts;
+    const unsigned dimz = (nz + (Ts-1))/Ts;
+    dim3 block(32,6,6);
+    dim3 grid(dimx, dimy, dimz);
 
-    for (int i = 0; i < iterations; ++i){
+    for (unsigned i = 0; i < iterations; ++i){
         if(i & 1){
-            sevenPointStencil_single_iter_tiled_sliding_read<T> <<< grid,block >>>(swap_out, start, nx, ny, nz);
+            sevenPointStencil_single_iter_tiled_sliding_read<<<grid,block>>>(swap_out, start, nx, ny, nz);
         }
         else {
-            sevenPointStencil_single_iter_tiled_sliding_read<T> <<< grid,block >>>(start, swap_out, nx, ny, nz);
+            sevenPointStencil_single_iter_tiled_sliding_read<<<grid,block>>>(start, swap_out, nx, ny, nz);
         }
     }
-    // we don't actually care to write the result to swap_out (in case the number of iterations is even)
+    CUDASSERT(cudaDeviceSynchronize());
 
+}
+
+template<int W>
+void stencil_1d_global_read(
+    const int * start,
+    int * out,
+    const unsigned len
+    )
+{
+    const int block = 1024;
+    const int grid = (len + (block-1)) / block;
+
+    breathFirst<W><<<grid,block>>>(start, out, len);
+    CUDASSERT(cudaDeviceSynchronize());
 }
 
 
 int main()
 {
     struct timeval t_startpar, t_endpar, t_diffpar;
-    int RUNS = 10;
+    int RUNS = 100;
 
-    const unsigned nx = 10;
-    const unsigned ny = 5000;
-    const unsigned nz = 5000;
-    const unsigned iterations = 30;
+    const unsigned nx = 1000;
+    const unsigned ny = 1000;
+    const unsigned nz = 3;
+    const unsigned iterations = 30; // must be an even number
 
-//    float* array1d = (float*)malloc(x*sizeof(float));
-//    float* array2d = (float*)malloc(x*y*sizeof(float));
-//    float* array3d = (float*)malloc(x*y*z*sizeof(float));
+    const unsigned total_size = nx*ny*nz;
 
-//    float* gpu_array1d;
-//    float* gpu_array2d;
+    float* array3d_s = (float*)malloc(total_size*sizeof(float));
+    float* array3d_ts = (float*)malloc(total_size*sizeof(float));
+    float* array3d_tsr = (float*)malloc(total_size*sizeof(float));
+
     float* gpu_array3d;
     float* gpu_array3d_2;
 
-//    CUDASSERT(cudaMalloc((void **) &gpu_array1d, x*sizeof(float)));
-//    CUDASSERT(cudaMalloc((void **) &gpu_array2d, x*y*sizeof(float)));
     CUDASSERT(cudaMalloc((void **) &gpu_array3d, 2*nx*ny*nz*sizeof(float)));
-    //CUDASSERT(cudaMalloc((void **) &gpu_array3d_2, nx*ny*nz*sizeof(float)));
-    gpu_array3d_2 = &gpu_array3d[nx*ny*nz];
+    gpu_array3d_2 = &(gpu_array3d[nx*ny*nz]);
     const float et_godt_primtal = 7.0;
 
-//    CUDASSERT(cudaMemset(gpu_array1d, et_godt_primtal, x*sizeof(float)));
-//    CUDASSERT(cudaMemset(gpu_array2d, et_godt_primtal, x*y*sizeof(float)));
     CUDASSERT(cudaMemset(gpu_array3d, et_godt_primtal, nx*ny*nz*sizeof(float)));
     CUDASSERT(cudaMemset(gpu_array3d_2, et_godt_primtal, nx*ny*nz*sizeof(float)));
 
+/*
     {
+        CUDASSERT(cudaDeviceSynchronize());
         cout << "## Benchmark GPU stupid ##" << endl;
 
         gettimeofday(&t_startpar, NULL);
@@ -155,9 +167,10 @@ int main()
         for(unsigned x = 0; x < RUNS; x++){
             sevenPointStencil(gpu_array3d, gpu_array3d_2, nx, ny, nz, iterations);
         }
-        CUDASSERT(cudaDeviceSynchronize());
 
         gettimeofday(&t_endpar, NULL);
+	CUDASSERT(cudaMemcpy(array3d_s, gpu_array3d_2, nx*ny*nz*sizeof(float), cudaMemcpyDeviceToHost))
+
         timeval_subtract(&t_diffpar, &t_endpar, &t_startpar);
         unsigned long elapsed = t_diffpar.tv_sec*1e6+t_diffpar.tv_usec;
             elapsed /= RUNS;
@@ -168,6 +181,7 @@ int main()
     }
 
     {
+        CUDASSERT(cudaDeviceSynchronize());
         cout << "## Benchmark GPU tiled sliding ##" << endl;
 
         gettimeofday(&t_startpar, NULL);
@@ -175,9 +189,10 @@ int main()
         for(unsigned x = 0; x < RUNS; x++){
             sevenPointStencil_tiledSliding(gpu_array3d, gpu_array3d_2, nx, ny, nz, iterations);
         }
-        CUDASSERT(cudaDeviceSynchronize());
 
         gettimeofday(&t_endpar, NULL);
+	CUDASSERT(cudaMemcpy(array3d_ts, gpu_array3d_2, nx*ny*nz*sizeof(float), cudaMemcpyDeviceToHost))
+
         timeval_subtract(&t_diffpar, &t_endpar, &t_startpar);
         unsigned long elapsed = t_diffpar.tv_sec*1e6+t_diffpar.tv_usec;
             elapsed /= RUNS;
@@ -187,16 +202,18 @@ int main()
 
     }
     {
-        cout << "## Benchmark GPU tiled sliding read ##" << endl;
+        CUDASSERT(cudaDeviceSynchronize());
+        cout << "## Benchmark GPU fully-tiled ##" << endl;
 
         gettimeofday(&t_startpar, NULL);
 
         for(unsigned x = 0; x < RUNS; x++){
-            sevenPointStencil_tiledSliding_read(gpu_array3d, gpu_array3d_2, nx, ny, nz, iterations);
+            sevenPointStencil_tiledSliding_fully(gpu_array3d, gpu_array3d_2, nx, ny, nz, iterations);
         }
-        CUDASSERT(cudaDeviceSynchronize());
 
         gettimeofday(&t_endpar, NULL);
+	CUDASSERT(cudaMemcpy(array3d_tsr, gpu_array3d_2, nx*ny*nz*sizeof(float), cudaMemcpyDeviceToHost))
+
         timeval_subtract(&t_diffpar, &t_endpar, &t_startpar);
         unsigned long elapsed = t_diffpar.tv_sec*1e6+t_diffpar.tv_usec;
             elapsed /= RUNS;
@@ -205,9 +222,48 @@ int main()
         printf("    mean elapsed time was: %lu.%03lu seconds\n", el_sec, el_mil_sec);
 
     }
+*/
+    CUDASSERT(cudaDeviceSynchronize());
+    {
+        const int len = 10000000;
+        //int* arr_in  = (int*)malloc(len*sizeof(int));
+        int* arr_out = (int*)malloc(len*sizeof(int));
+        const int a_number = 7;
 
-//    cudaFree(gpu_array1d);
-//    cudaFree(gpu_array2d);
+        int* gpu_array_in;
+        int* gpu_array_out;
+        CUDASSERT(cudaMalloc((void **) &gpu_array_in, 2*len*sizeof(int)));
+        gpu_array_out = &(gpu_array_in[len]);
+        CUDASSERT(cudaMemset(gpu_array_in, a_number, 2*len*sizeof(float)));
+
+        CUDASSERT(cudaDeviceSynchronize());
+        cout << "## Benchmark GPU 1d global-mem ##" << endl;
+
+        gettimeofday(&t_startpar, NULL);
+
+        for(unsigned x = 0; x < RUNS; x++){
+            stencil_1d_global_read<10000>(gpu_array_in, gpu_array_out, len);
+        }
+
+        CUDASSERT(cudaDeviceSynchronize());
+        gettimeofday(&t_endpar, NULL);
+	CUDASSERT(cudaMemcpy(arr_out, gpu_array_out, len*sizeof(float), cudaMemcpyDeviceToHost))
+
+        timeval_subtract(&t_diffpar, &t_endpar, &t_startpar);
+        unsigned long elapsed = t_diffpar.tv_sec*1e6+t_diffpar.tv_usec;
+        elapsed /= RUNS;
+        printf("    mean elapsed time was: %lu microseconds\n", elapsed);
+
+        free(arr_out);
+        cudaFree(gpu_array_in);
+        cudaFree(gpu_array_out);
+    }
+
+
+    free(array3d_s);
+    free(array3d_ts);
+    free(array3d_tsr);
+
     cudaFree(gpu_array3d);
     cudaFree(gpu_array3d_2);
 
