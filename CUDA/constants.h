f9beea1bae9a6f123dc4ae9564a8fb1fc0632342
Christian Virt <christianvirt@gmail.com>
2021-03-01 15:49:53 +0100

    3d tests

--- CUDA/constants.h
+++ CUDA/constants.h
@@ -3,6 +3,12 @@
 
 #define BLOCKSIZE 1024
 #define SQ_BLOCKSIZE 32
+
+//3d blocksizes
+#define X_BLOCK 32
+#define Y_BLOCK 8
+#define Z_BLOCK 4
+
 #define BOUND(i,max_ix) (min((max_ix),max(0,(i))))
 
 __constant__ int ixs[BLOCKSIZE*3];
